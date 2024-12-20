2efbfc841ff7d45bf988dcb87d269cc71bfa3750
yamenzk <yz.kh@icloud.com>
2024-12-20 22:02:27 +0100

    Major: Removed PV, Project Bill

--- rua_company/rua_company/doctype/payment_voucher/test_payment_voucher.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Yamen Zakhour and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestPaymentVoucher(FrappeTestCase):
-	pass
--- rua_company/rua_company/doctype/project_bill/project_bill.js
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2024, Yamen Zakhour and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Project Bill', {
-
-});
